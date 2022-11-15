810a23c7e43e3e36349b4855a196a18d34ab2459
robinetta <alxr19@gmail.com>
2022-11-14 21:29:19 -0500

    basic layout setup

---
+++ js/app.js
@@ -0,0 +1,28 @@
+
+// const gameBoard = (() =>{
+//     let board = ["x","0","x","x","0","x","x","0","x"]
+//     function updateBoard(board){
+//         for (let i = 0; i > board.length; i++){
+
+//         }
+//     } 
+//     return{
+//         updateBoard,
+//         board
+//     }
+// })();
+
+// gameBoard.updateBoard()
+
+
+const displayController = (() => {
+    const gameBoard = document.querySelector(".gameboard")
+    const playersqure = document.createElement("div")
+})()
+
+function createPlayer(name){
+    const playerName = name
+    return{
+        playerName
+    }
+}
